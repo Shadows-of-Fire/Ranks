e1ec3eff264df1b5074757576292648690febe82
Shadows_of_Fire <Bward7864@gmail.com>
2020-10-06 00:02:50 -0400

    Add backup task

--- src/main/java/shadows/ranks/Ranks.java
+++ src/main/java/shadows/ranks/Ranks.java
@@ -13,6 +13,8 @@ import java.util.UUID;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.common.io.Files;
+
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
 import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
 import net.minecraftforge.common.MinecraftForge;
@@ -35,6 +37,7 @@ public class Ranks {
 	public static final String VERSION = "1.0.2";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
+	static Object fileLock = new Object();
 	static Object2LongMap<UUID> TIMES = new Object2LongOpenHashMap<>();
 	static File propFile;
 	static long ticks = 0;
@@ -74,36 +77,58 @@ public class Ranks {
 	@SubscribeEvent
 	public void tick(WorldTickEvent e) {
 		if (e.phase == Phase.END) {
-			if (++ticks % 36000 == 0) {
-				new Thread(Ranks::serializeTimes, "Ranks serialization thread").start();
+			if (++ticks % 24000 == 0) {
+				new Thread(Ranks::serializeAndBackup, "Ranks serialization thread").start();
 			}
 		}
 	}
 
-	private static synchronized void serializeTimes() {
-		Properties props = new Properties();
-		for (Object2LongMap.Entry<UUID> e : TIMES.object2LongEntrySet()) {
-			props.setProperty(e.getKey().toString(), String.valueOf(e.getLongValue()));
-		}
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(propFile))) {
-			props.store(writer, "Ranks user logged in time.");
-		} catch (IOException ex) {
-			ex.printStackTrace();
+	private static void serializeTimes() {
+		synchronized (fileLock) {
+			Properties props = new Properties();
+			for (Object2LongMap.Entry<UUID> e : TIMES.object2LongEntrySet()) {
+				props.setProperty(e.getKey().toString(), String.valueOf(e.getLongValue()));
+			}
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter(propFile))) {
+				props.store(writer, "Ranks user logged in time.");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 		}
 	}
 
-	private static void deserializeTimes() {
-		Properties props = new Properties();
-		try (BufferedReader reader = new BufferedReader(new FileReader(propFile))) {
-			props.load(reader);
-		} catch (IOException ex) {
-			ex.printStackTrace();
+	private static void backup() {
+		synchronized (fileLock) {
+			File backupDir = new File(propFile.getParentFile(), "rank_backups");
+			if (!backupDir.exists()) backupDir.mkdir();
+			File out = new File(backupDir, String.format("%s-%2$tY%2$tm%2$td-%2$tH%2$tM%2$tS.properties", "ranks", System.currentTimeMillis()));
+			try {
+				Files.copy(propFile, out);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		TIMES.clear();
-		for (Map.Entry<Object, Object> e : props.entrySet()) {
-			UUID id = UUID.fromString((String) e.getKey());
-			long ticks = Long.parseLong((String) e.getValue());
-			TIMES.put(id, ticks);
+	}
+
+	private static void serializeAndBackup() {
+		serializeTimes();
+		backup();
+	}
+
+	private static void deserializeTimes() {
+		synchronized (fileLock) {
+			Properties props = new Properties();
+			try (BufferedReader reader = new BufferedReader(new FileReader(propFile))) {
+				props.load(reader);
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			TIMES.clear();
+			for (Map.Entry<Object, Object> e : props.entrySet()) {
+				UUID id = UUID.fromString((String) e.getKey());
+				long ticks = Long.parseLong((String) e.getValue());
+				TIMES.put(id, ticks);
+			}
 		}
 	}
 
